6ffc149031128af7991b6bae628423c33800f531
Rada Bogdan Raul <raoul_ice@yahoo.com>
2015-07-08 00:33:35 +0300

    refactor

--- lib/capistrano/gitflow.rb
+++ lib/capistrano/gitflow.rb
@@ -7,7 +7,7 @@ self.extend CapistranoGitFlow::Helper
 include CapistranoGitFlow::Helper
 
 
-if using_cap3?
+if gitflow_using_cap3?
   require  File.join(File.dirname(__FILE__), 'tasks', 'gitflow')
 else
   require File.join(File.dirname(__FILE__), 'gitflow','legacy', 'gitflow')
