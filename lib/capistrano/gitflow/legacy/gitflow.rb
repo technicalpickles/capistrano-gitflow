5dcf8b963100636db94e1b270b5ba49f64e35875
Rada Bogdan Raul <raoul_ice@yahoo.com>
2016-06-28 17:38:28 +0300

    update the legacy helper

--- lib/capistrano/gitflow/legacy/gitflow.rb
+++ lib/capistrano/gitflow/legacy/gitflow.rb
@@ -3,6 +3,9 @@ require 'capistrano'
 module Capistrano
   class Gitflow
     def self.load_into(capistrano_configuration)
+      capistrano_configuration.class.class_eval do
+        include CapistranoGitFlow::BaseHelper
+      end
       capistrano_configuration.load  File.join(File.dirname(File.dirname(File.dirname(__FILE__))), 'tasks', 'gitflow.rb')
     end
   end
