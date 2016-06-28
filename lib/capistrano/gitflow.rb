e46aac13da0df22691bf681f8a4de5c1a2aee100
Rada Bogdan Raul <raoul_ice@yahoo.com>
2016-06-28 17:01:03 +0300

    update helper

--- lib/capistrano/gitflow.rb
+++ lib/capistrano/gitflow.rb
@@ -3,8 +3,21 @@ require 'stringex'
 Gem.find_files('capistrano/gitflow/helpers/**/*.rb').each { |path| require path }
 require 'capistrano/version'
 
+
+module CapistranoGitFlow
+  class << self
+    def cap_major_version
+      defined?(Capistrano::VERSION) ? Capistrano::VERSION.to_s.split('.').first.to_i : nil
+    end
+
+    def is_using_cap3?
+      cap_major_version.to_i > 0 && cap_major_version >= 3
+    end
+  end
+end
+
 unless defined?(Sinatra)
-  if CapistranoGitFlow::Helper::ClassMethods.gitflow_using_cap3?
+  if CapistranoGitFlow.is_using_cap3?
     require 'capistrano/all'
     require  File.join(File.dirname(__FILE__), 'tasks', 'gitflow')
   else
