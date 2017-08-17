41dc0f522e0c2909959ab4fee2dd6c09691c5f23
Rada Bogdan <raoul_ice@yahoo.com>
2017-08-17 09:41:03 +0300

    bump version

--- lib/capistrano/gitflow/version.rb
+++ lib/capistrano/gitflow/version.rb
@@ -5,10 +5,11 @@ module  CapistranoGitFlow # Returns the version of the currently loaded Rails as
 
   module VERSION
     MAJOR = 1
-    MINOR = 5
-    TINY = 7
+    MINOR = 6
+    TINY = 0
     PRE = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
 end
+
